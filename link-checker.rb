f9103c4df3fb7b47847ad8fb72ac0121662c324d
John Manoogian III <jm3@jm3.net>
2009-09-06 14:29:28 -0700

    cleanup for v1

--- link-checker.rb
+++ link-checker.rb
@@ -1,17 +1,6 @@
 #!/opt/local/bin/ruby
 # A reentrant link-checker for delicious power-users, stress-tested with stores of 12,000+ links.
 # by jm3 (John Manoogian III)
-#
-# Features:
-# * Resilient to control-c interrupts, will resume checking where left off.
-# * Delicious API responses cached locally so they need only be retrieved once.
-# * Dead links persisted in a Moneta cache for you to deal with as you please, e.g.
-#
-# % xatttr .moneta_cache/xattr_cache | xargs -n1 -I foo curl "http://api.delicious.com/delete?url=foo"
-#
-# Todo:
-# * cool animated progress indicator with ncurses
-# * test sax parsing to see if it's a faster load
 
 require 'rubygems'
 require 'curb'
@@ -21,10 +10,6 @@ require 'moneta/xattr'
 require 'net/https'
 require 'rexml/document'
 include REXML
-
-# no longer needed:
-#require "ridiculous"
-#include Ridiculous
 
 def pointer_key
   'index_pointer'
